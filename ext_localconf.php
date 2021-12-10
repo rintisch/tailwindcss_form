79399e317dcc5d556e4730c39e060f2ce9b98eea
rintisch <rintisch@stadtwerk.org>
2021-12-10 10:42:06 +0100

    Add TypoScript to backend

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,20 @@ defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterBuildingFinished'][]
     = \Rintisch\TailwindcssForm\Hooks\StepIndicator::class;
+
+if (TYPO3_MODE === 'BE') {
+    /**
+     * Register custom EXT:form configuration
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        trim('
+                module.tx_form {
+                    settings {
+                        yamlConfigurations {
+                            90 = EXT:tailwindcss_form/Configuration/Form/CustomFormSetup.yaml
+                        }
+                    }
+                }
+            ')
+    );
+}
